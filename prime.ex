c579560190d4a103288580ed3606da31b357ffe4
Lelick102 <olga.medintseva@yandex.ua>
2019-12-07 15:57:16 -0400

    3 changes add is_Prime. It's work

--- prime.ex
+++ prime.ex
@@ -19,4 +19,31 @@ defmodule Prime do
   def sum(a,b) do
     a+b
   end
+
+  def inputt() do
+    input=IO.gets("N>")
+    r=String.trim(input)
+    String.to_integer(r)
+  end
+
+  def is_Prime(limit) do 
+    is_Prime(2,limit)
+  end
+
+  defp is_Prime(count,limit) when count<=limit do
+    if rem(limit,count)==0 do
+      is_Prime(count+1,limit)+1
+    else
+      is_Prime(count+1,limit)
+    end
+  end
+
+  defp is_Prime(count,limit) when count>0 do
+    0
+  end
+
+  def add_list(n) do
+    [n]
+  end
+
 end
