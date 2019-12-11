15e5e33a90ce7956bc7f8002ad2f46429a97524c
Olga <olga.medintseva@yandex.ua>
2019-12-11 20:54:22 +0300

    Add router and GenServer

---
+++ server.ex
@@ -0,0 +1,29 @@
+defmodule Server do
+  use GenServer
+
+  def start_task do
+    GenServer.call(__MODULE__, :start_task)
+  end
+ 
+  def handle_call(:start_task, _from, %{ref: ref} = state) when is_reference(ref) do
+    {:reply, :ok, state}
+  end
+
+  def handle_call(:start_task, _from, %{ref: nil} = state) do
+    task =
+      Task.Supervisor.async_nolink(RouterTasks, fn ->
+        ...
+      end)
+
+    {:reply, :ok, %{state | ref: task.ref}}
+  end
+
+  def handle_info({ref, answer}, %{ref: ref} = state) do
+    Process.demonitor(ref, [:flush])
+    {:noreply, %{state | ref: nil}}
+  end
+
+  def handle_info({:DOWN, ref, :process, _pid, _reason}, %{ref: ref} = state) do
+    {:noreply, %{state | ref: nil}}
+  end
+end
