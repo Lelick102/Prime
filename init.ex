dfb2592a5a9a56b07cb763a96a03656148378b25
Lelick102 <olga.medintseva@yandex.ua>
2019-12-13 19:15:18 -0400

    Add Init

---
+++ init.ex
@@ -0,0 +1,10 @@
+defmodule Init do
+    def initNode(node) do
+        Node.start(node, :longnames, 9000)
+        Node.set_cookie(:secret)
+    end
+    
+    def initNodes do
+        [{RouterTasks, :"comp1@172.17.0.3"},{RouterTasks, :"comp2@172.17.0.4"}, {RouterTasks, :"comp3@172.17.0.5"}]
+    end
+end
