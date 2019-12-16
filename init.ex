3654935d9c3f2057b4b193f287c6e68cc90c170e
Lelick102 <olga.medintseva@yandex.ua>
2019-12-16 13:42:41 -0400

    Init and StartTask

--- init.ex
+++ init.ex
@@ -7,4 +7,8 @@ defmodule Init do
     def initNodes do
         [{RouterTasks, :"comp1@172.17.0.3"},{RouterTasks, :"comp2@172.17.0.4"}, {RouterTasks, :"comp3@172.17.0.5"}]
     end
+    
+    def initFile(path) do
+        File.open!(path, [:read,:utf8,:write])
+    end
 end
--- startTask.ex
+++ startTask.ex
@@ -1,15 +1,14 @@
 defmodule StartTask do
-   def start_Task(n,b,a,path) do
-      f=Prime.fileInit(path)
-      Task.Supervisor.async(n, Prime, :check_Prime_Numbers, [b,a,f])
+   def start_Task(n,b,a,file) do
+      Task.Supervisor.async(n, Prime, :check_Prime_Numbers, [b,a,file])
    end
    
-   def start_Count([head|tail],b,e,path) do
-       start_Task(head, b, round(e/length [head|tail]), path)
-       start_Count(tail,round(e/length [head|tail]),e, path)
+   def start_Count([head|tail],b,e,file) do
+       start_Task(head, b, round(e/length [head|tail]), file)
+       start_Count(tail,round(e/length [head|tail]),e, file)
    end
 
-   def start_Count([], b, e, path) do
+   def start_Count([], b, e, file) do
       true
    end
 
