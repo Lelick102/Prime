a9f42b1ce6bec074aaf36a2cfb5e05ddb399ec85
Olga <olga.medintseva@yandex.ua>
2020-01-30 00:08:44 +0300

    cofig

--- config.ex
+++ config.ex
@@ -1,12 +1,13 @@
 defmodule Config do
 
-  def server do :"server@172.17.0.2"  end
+  def server do :"server@172.17.0.4"  end
   def client do :"client@172.17.0.3" end
   def node1 do :"node1@172.17.0.4" end
 
     def init(name,cookie) do
        Node.start name
        Node.set_cookie cookie
+	   Task.Supervisor.start_link(name: RouterTasks)
     end
 end
    
