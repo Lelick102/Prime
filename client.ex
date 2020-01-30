eea475e0f4ab1a0f3f1a2fd55e837d62bb324be7
Olga <olga.medintseva@yandex.ua>
2020-01-30 09:51:22 +0300

    client 2

--- client.ex
+++ client.ex
@@ -1,6 +1,6 @@
 defmodule Client do
     def create(name, context) do
-	    task=Task.Supervisor.async({RouterTasks,:"server@172.17.0.4"},Server, :create, [name,context])
+	    task=Task.Supervisor.async({RouterTasks,:"server@172.17.0.5"},Server, :create, [name,context])
 		Task.await(task)
 	end
 end
