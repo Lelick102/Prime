6964c6a618cc9964cf607ab64b0b98197602295f
Olga <olga.medintseva@yandex.ua>
2020-01-30 19:25:32 +0300

    config all8

--- server.ex
+++ server.ex
@@ -13,7 +13,7 @@ defmodule Server do
 	end
    
 	def start_create([head|tail],name,context, count) do 
-		start_Task(head, name, fn->String.slice(context, count .. length([head|tail])) end)
+		start_Task(head, name, context)
 		start_create(tail,name,context,count)
 	end
 
