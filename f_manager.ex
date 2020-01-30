f90746ec93cfb2a67ca7060369b4de3efb2ff2c6
Olga <olga.medintseva@yandex.ua>
2020-01-30 20:17:37 +0300

    config all8

--- f_manager.ex
+++ f_manager.ex
@@ -1,16 +1,18 @@
 defmodule F_manager do
 	def insert_file(name,context) do
 		try do
-		    data=""
 			if is_file_existense(context) do
 				{data}=File.read!(context)
+				{:ok,file}=File.open(name,[:append])
+				IO.write(file, data)
+				File.close(file)
 			else
 				data=context
+				{:ok,file}=File.open(name,[:append])
+				IO.write(file, data)
+				File.close(file)
 			end
 				
-			{:ok,file}=File.open(name,[:append])
-			IO.write(file, data)
-			File.close(file)
 			true
 		rescue
 			error->error
