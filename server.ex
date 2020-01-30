c7659a96ce462b1f1927ad802a763015cf952aaf
Olga <olga.medintseva@yandex.ua>
2020-01-30 09:36:24 +0300

    server changes

--- server.ex
+++ server.ex
@@ -1,10 +1,11 @@
 defmodule Server do
 	def create(name,context) do
 		try do
-			for item <- F_manager.devide_str(context) do
-				task=Task.Supervisor.async({RouterTasks,:"root@172.17.0.5"},F_manager, :insert_file, [name,context])
-				Task.await(task)
-			end
+			#for item <- F_manager.devide_str(context) do
+				#task=Task.Supervisor.async({RouterTasks,:"root@172.17.0.5"},F_manager, :insert_file, [name,context])
+				#Task.await(task)
+			Router.route("r", F_manager, :insert_file, [name,context])
+			#end
 		rescue
 			error->error
 		end
