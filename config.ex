040446a36211fef727cc11687fe00ab82a4a5c75
Olga <olga.medintseva@yandex.ua>
2020-01-30 09:10:52 +0300

    config 5

--- config.ex
+++ config.ex
@@ -3,6 +3,7 @@ defmodule Config do
   def server do :"server@172.17.0.4"  end
   def client do :"client@172.17.0.3" end
   def node1 do :"node1@172.17.0.4" end
+  def root do :"root@172.17.0.5"
 
     def init(name,cookie) do
        Node.start name
