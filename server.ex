2f3f6550538d3e0cf73cbaa4c679dc2367f9c994
Olga <olga.medintseva@yandex.ua>
2020-01-30 11:54:19 +0300

    server10

--- server.ex
+++ server.ex
@@ -2,8 +2,10 @@ defmodule Server do
 	def create(name,context) do
 		try do
 		    count=0
+			l=["r","r"]
 		    data=F_manager.devide_str(context)
-			for item <- ["r","r"] do
+			for item <- l do
+			    #s=String.slice context, count, context/length
 				Router.route(item, F_manager, :insert_file, [name,context])
 			end
 		rescue
