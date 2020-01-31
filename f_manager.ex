0c4f8d2c4fc523a8d2c12ae4147711897660cfd2
Olga <olga.medintseva@yandex.ua>
2020-01-31 21:54:21 +0300

    add fiels

--- f_manager.ex
+++ f_manager.ex
@@ -1,7 +1,7 @@
 defmodule F_manager do
 	def insert_file(name,context) do
 		try do
-			{:ok,file}=File.open(name,[:append])
+			{:ok,file}=File.open(name,[:write])
 			IO.write(file, context)
 			File.close(file)
 			true
