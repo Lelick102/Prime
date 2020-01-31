aadf542e5f37f4e4a6a8e487b4fc03a27f4d4579
Olga <olga.medintseva@yandex.ua>
2020-01-31 08:28:10 +0300

    config all8

--- server.ex
+++ server.ex
@@ -2,7 +2,7 @@ defmodule Server do
 	def create(name,context) do
 		try do
 			nodes=["r","n"]
-			data=get_data_client(context)
+			data=get_data_client()
 			start_create(nodes,name,data,0)			
 		rescue
 			error->error
