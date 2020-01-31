8568262ea25a506bd76feba78959533d005b43b9
Olga <olga.medintseva@yandex.ua>
2020-01-31 09:13:16 +0300

    config all8

--- server.ex
+++ server.ex
@@ -14,7 +14,7 @@ defmodule Server do
 	end
    
 	def start_create([head|tail],name,data,count) do  
-		start_Task(head, name, String.slice(data,count,round(String.length(data)/length[head|tail]))
+		start_Task(head, name, String.slice(data,count,round(String.length(data)/length[head|tail])))
 		start_create(tail, name, data, count+round(String.length(data)/length[head|tail]),String.length(data))
 	end
 
